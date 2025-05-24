ac33794bff2f3da870ddc35ab15b8dbcde1882b7
hoangphatdev <latruonghoangphat@gmail.com>
2025-05-25 00:48:46 +0700

    initial project create by vite(react), spring initialize (spring)

---
+++ autoparts_api/src/main/java/com/vku/autoparts_api/AutopartsApiApplication.java
@@ -0,0 +1,13 @@
+package com.vku.autoparts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopartsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopartsApiApplication.class, args);
+	}
+
+}
